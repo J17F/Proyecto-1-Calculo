910e8b2b58b9cb17098a70098e0183182ec92ce5
Edgard Decena <edecena@gmail.com>
2021-01-26 13:11:12 -0400

    Primer commit.

---
+++ proyecto_1_calculo.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Proyecto sobre Python y el workflow Pull-Request en GitHub.
+
+Cada participante debe completar su función y luego solicitar el Pull-Request.
+"""
+
+
+def Ashly_Scarpati(a, b, c):
+    """
+    Recibe como parámetros los valores a, b, c y retorna el mínimo de a, b y c.
+    """
+    return
+
+
+def Elder_Gomes(lista):
+    """
+    Recibe como parámetro una lista y retorna el mínimo valor de la lista.
+    """
+    return
+
+
+def Hector_Salazar(tupla):
+    """
+    Recibe como parámetro una tupla y retorna el primer elemento de la tupla.
+    """
+    return
+
+
+def Jose_Gomez(cadena):
+    """
+    Recibe como paŕametro una cadena y retorna el número de caracteres de la cadena.
+    """
+    return
+
+
+def Lewis_Narvaez(tupla):
+    """
+    Recibe como parámetro una tupla y retorna el último elemento de la tupla.
+    """
+    return
+
+
+def Luisana_Hernandez(a, b, c):
+    """
+    Recibe como parámetros a, b, c y retorna el máximo de a, b, c.
+    """
+    return
+
+
+def Lusandre_Marcano(conjunto):
+    """
+    Recibe como parámetros un conjunto y retorna el máximo del conjunto.
+    """
+    return
